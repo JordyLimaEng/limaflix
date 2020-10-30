570cca1dd204051a6b588d8efe76cecf1d2cebc5
Jordy Lima <jordy.sousa@eng.ci.ufpb.br>
2020-10-30 16:28:46 -0300

    Initial commit
    Arquivos de  vídeo não inclusos

---
+++ client/src/App.js
@@ -0,0 +1,68 @@
+import React, { Component, useState, useEffect } from 'react';
+import api from './services/api'
+import "./styles.css";
+
+import Modal from "react-modal";
+
+import { Button } from "react-bootstrap";
+
+
+
+import { Player, ControlBar, ClosedCaptionButton } from 'video-react';
+import 'video-react/dist/video-react.css';
+
+export default function App() {
+  const [filmes, setFilmes] = useState([]);
+  const [assistir, setAssistir] = useState();
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleModal() {
+    setIsOpen(!isOpen);
+  }
+
+  async function getFilmes() {
+    try {
+      const res = await api.get("/filmes")
+      setFilmes(res.data.filmes)
+    } catch (e) {
+      console.error(e.message)
+    }
+  };
+
+  function HandleClick(name) {
+    toggleModal();
+    setAssistir(name);
+  }
+
+  return (
+    <>
+      <p className="App-intro">LIMAFLIX</p>
+      <button onClick={() => getFilmes()}>Catálogo</button>
+      {filmes.map(i => (
+        <p>
+          {i.titulo} <Button onClick={() => HandleClick(i.titulo)}>Assistir</Button>
+        </p>
+      ))}
+
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={toggleModal}
+        contentLabel="My dialog"
+        className="mymodal"
+        overlayClassName="myoverlay"
+        closeTimeoutMS={500}
+      >
+        <div>{assistir}</div>
+        <div className='player-wrapper'>
+          <Player className='react-player'
+            width='100%'
+            height='100%'>
+            <source src={`http://localhost:4001/filmes/${assistir}`} />
+          </Player>
+        </div>
+        <Button variant="danger" onClick={toggleModal}>Fechar</Button>
+      </Modal>
+    </>
+  );
+}
+
---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 4001;
+
+const fs = require('fs');
+
+function res_filmes(){
+  let filmes = "./filmes";
+  let dir_filmes = fs.readdirSync(filmes);
+  let filmes_list = {filmes:[]};
+  
+  dir_filmes.forEach((file) => {
+    filmes_list.filmes.push(
+      {
+        titulo : file
+      }
+    );
+  });
+  
+  return filmes_list;
+}
+
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
+app.get('/filmes', (req, res) => {
+    res.send(res_filmes())
+});
+
+app.get('/filmes/:movieName', (req, res) => {
+  const { movieName } = req.params;
+  const movieFile = `./filmes/${movieName}`;
+  fs.stat(movieFile, (err, stats) => {
+    if (err) {
+      console.log(err);
+      return res.status(404).end('<h1>Movie Not found</h1>');
+    }
+    // Variáveis necessárias para montar o chunk header corretamente
+    const { range } = req.headers;
+    const { size } = stats;
+    const start = Number((range || '').replace(/bytes=/, '').split('-')[0]);
+    const end = size - 1;
+    const chunkSize = (end - start) + 1;
+    // Definindo headers de chunk
+    res.set({
+      'Content-Range': `bytes ${start}-${end}/${size}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunkSize,
+      'Content-Type': 'video/mp4'
+    });
+    // É importante usar status 206 - Partial Content para o streaming funcionar
+    res.status(206);
+    // Utilizando ReadStream do Node.js
+    // Ele vai ler um arquivo e enviá-lo em partes via stream.pipe()
+    const stream = fs.createReadStream(movieFile, { start, end });
+    stream.on('open', () => stream.pipe(res));
+    stream.on('error', (streamErr) => res.end(streamErr));
+  });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
