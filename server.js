ee09b3bd3a07429891494ca5120d1f8395b3fae5
Jordy Lima <jordy.sousa@eng.ci.ufpb.br>
2020-11-05 11:35:55 -0300

    Atualizando com layout do Netflix

--- server.js
+++ server.js
@@ -5,39 +5,68 @@ const app = express();
 const port = process.env.PORT || 4001;
 
 const fs = require('fs');
+const path = require('path');
 
-function res_filmes(){
-  let filmes = "./filmes";
+function res_filmes() {
+  let filmes = "./client/public/files";
   let dir_filmes = fs.readdirSync(filmes);
-  let filmes_list = {filmes:[]};
-  
+  let thumbs = [];
+  let videos = [];
+  let arquivos = [];
+  let filmes_list = { filmes: [] };
+
   dir_filmes.forEach((file) => {
-    filmes_list.filmes.push(
-      {
-        titulo : file
-      }
-    );
+    const ext = path.extname(file);
+    
+    if(ext === ".png" || ext === ".jpg" || ext === ".webp"){
+      thumbs.push(file);
+      arquivos.push(file);
+    }else if(ext === ".mp4" || ext === ".mkv"){
+      videos.push(file);
+      arquivos.push(file);
+    }
+
+    // filmes_list.filmes.push(
+    //     {
+    //       titulo: file,
+    //       thumb: file
+    //     }
+    //   );
   });
-  
-  return filmes_list;
+
+  let index = 0;
+  arquivos.forEach((file) => {
+    const ext = path.extname(file);        
+    if(ext === ".png" || ext === ".jpg" || ext === ".webp"){
+      filmes_list.filmes.push(
+        {
+          titulo: arquivos[index+1],
+          thumb: arquivos[index]
+        }
+      );
+    }
+    index++;
+  });
+
+return filmes_list;
 }
 
 app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
-    next();
+  //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+  res.header("Access-Control-Allow-Origin", "*");
+  //Quais são os métodos que a conexão pode realizar na API
+  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+  app.use(cors());
+  next();
 });
 
 app.get('/filmes', (req, res) => {
-    res.send(res_filmes())
+  res.send(res_filmes())
 });
 
 app.get('/filmes/:titulo', (req, res) => {
   const { titulo } = req.params;
-  const arquivo = `./filmes/${titulo}`;
+  const arquivo = `./client/public/files/${titulo}`;
   fs.stat(arquivo, (err, stats) => {
     if (err) {
       console.log(err);
@@ -60,7 +89,7 @@ app.get('/filmes/:titulo', (req, res) => {
     res.status(206);
     // Utilizando ReadStream do Node.js
     // Ele vai ler um arquivo e enviá-lo em partes via stream.pipe()
-    const stream = fs.createReadStream(movieFile, { start, end });
+    const stream = fs.createReadStream(arquivo, { start, end });
     stream.on('open', () => stream.pipe(res));
     stream.on('error', (streamErr) => res.end(streamErr));
   });
